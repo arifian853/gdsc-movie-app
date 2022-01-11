52aa45d241c75f1879b58a8c5fe4b6a467a9906e
Arifian Saputra <arifiansaputra43@gmail.com>
2022-01-11 12:34:14 +0700

    Updates

---
+++ precache-manifest.d2a60e3b65dcd79f92c8b1ff0c7a026b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a6c2b67706c6f62fb030f0a92e722d7",
+    "url": "/gdsc-movie-app/index.html"
+  },
+  {
+    "revision": "f42c5f910f6608b65220",
+    "url": "/gdsc-movie-app/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "79d200c7fff902e6f073",
+    "url": "/gdsc-movie-app/static/css/main.412b7d24.chunk.css"
+  },
+  {
+    "revision": "f42c5f910f6608b65220",
+    "url": "/gdsc-movie-app/static/js/2.53013f4d.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/gdsc-movie-app/static/js/2.53013f4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7884238d07802802845d",
+    "url": "/gdsc-movie-app/static/js/3.b8a3dff6.chunk.js"
+  },
+  {
+    "revision": "79d200c7fff902e6f073",
+    "url": "/gdsc-movie-app/static/js/main.d6c91193.chunk.js"
+  },
+  {
+    "revision": "1296733dc0409c286274",
+    "url": "/gdsc-movie-app/static/js/runtime-main.a56bc4fa.js"
+  }
+]);
